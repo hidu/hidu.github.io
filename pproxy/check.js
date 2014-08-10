a9674d0a36d66473bdf4281db99ac4c8232f1464
duwei <duv123@gmail.com>
2014-08-10 22:51:08 +0800

    update

--- pproxy/check.js
+++ pproxy/check.js
@@ -4,7 +4,7 @@
  */
 (function(){
 	var version=((window.pproxy_version||"")+"").split(".");
-	var version_last_str="0.4.1";
+	var version_last_str="0.4.2";
 	var version_last=version_last_str.split(".");
 	var has_new_version=false;
 	for(var i in version_last){
@@ -14,7 +14,7 @@
 		}
 	}
 	var html="";
-	html="The last version is : " + "<font color=red>"+version_last_str+"</font>";
+	html="The last version is : " + "<font color=red>"+version_last_str+"</font>,<font color=blue>websocket sup!</font>";
 //	if(has_new_version){
 //		html+=""
 //	}
