b7830b35b8634ebcdd28aaed67ea38666d125124
hidu <duv123+git@gmail.com>
2015-09-21 21:18:10 +0800

    check

--- cmd2http/check.js
+++ cmd2http/check.js
@@ -5,6 +5,7 @@
 	var version=((window.cmd2http_version||"")+" ").split(" ")[0];
 	var version_last_str="0.2.1";
 	var version_last=version_last_str.split(".");
+	console && console.log("current version:",version)
 	var has_new_version=false;
 	for(var i in version_last){
 		if(parseInt(version_last[i]) >parseInt(version[i]||0)){
@@ -13,7 +14,7 @@
 		}
 	}
 	var html="";
-	html="last version: " + "<font color=red>"+version_last_str+"</font>";
+	html="<font color=red>there is a new version: <b>"+version_last_str+"</b></font><br/><br/>";
 	var div=document.getElementById("new_version_slot");
 	div.innerHTML=html
 })();
